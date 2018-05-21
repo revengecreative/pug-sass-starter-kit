2230f2a7ec9410156ea1997bdfd07d905c63d5a9
Davide Palazzo <kans@hotmail.it>
2018-05-21 10:03:38 +1000

    Removed semicolons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,33 +9,33 @@ import sass from 'gulp-sass'
 import sourcemaps from 'gulp-sourcemaps'
 import uglify from 'gulp-uglify'
 
-const assets ='./src/assets';
-const dist ='./dist';
+const assets ='./src/assets'
+const dist ='./dist'
 const reload = browserSync.reload
-const rootDir ='./';
-const src ='./src';
-const views ='./src/views';
+const rootDir ='./'
+const src ='./src'
+const views ='./src/views'
 
 gulp.task('serve', ['sass','templates'], () => {
-    let files = ['**/*.{png,jpg,gif}'];
+    let files = ['**/*.{png,jpg,gif}']
     browserSync.init(files, {
       server: { baseDir: dist },
       notify: false,
       port: 3019,
       browser: 'firefoxdeveloperedition' // pick your favourite browser
-    });
-    gulp.watch(`${src}/assets/sass/**/*.scss`, ['sass']);
-    gulp.watch('**/*.html').on('change', reload);
-    gulp.watch('**/*.php').on('change', reload);
-    gulp.watch(`${views}/**/*.pug`,['pug-watch']);
-});
+    })
+    gulp.watch(`${src}/assets/sass/**/*.scss`, ['sass'])
+    gulp.watch('**/*.html').on('change', reload)
+    gulp.watch('**/*.php').on('change', reload)
+    gulp.watch(`${views}/**/*.pug`,['pug-watch'])
+})
 
 gulp.task('sass', () => {
   return gulp.src(`${src}/assets/sass/**/*.scss`)
     .pipe(plumber({
       errorHandler: function(error) {
-        console.log(error.message);
-        this.emit('end');
+        console.log(error.message)
+        this.emit('end')
       }
     }))
     .pipe(sourcemaps.init())
@@ -44,8 +44,8 @@ gulp.task('sass', () => {
     .pipe(sourcemaps.write('maps', {addComment: true}))
     .pipe(plumber.stop())
     .pipe(gulp.dest(`${dist}/assets/css/`))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('templates', () => {
   return gulp.src(`${views}/**/!(_)*.pug`)
@@ -53,14 +53,14 @@ gulp.task('templates', () => {
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest(dist));
-});
+    .pipe(gulp.dest(dist))
+})
 
 gulp.task('buildAssets', () => {
   return gulp.src([`${assets}/**`, `!${assets}/libraries{,/**}`, `!${assets}/sass{,/**}`])
     .pipe(gulp.dest(`${dist}/assets`))
 })
 
-gulp.task('pug-watch', ['templates'], reload);
+gulp.task('pug-watch', ['templates'], reload)
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve'])
